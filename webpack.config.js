79093a249f9205fac4f05f16d463d8a8c6ed5977
ev <e.a.vasiliev@gmail.com>
2022-01-26 02:31:34 +0300

    Add docker-compose

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     open: false,
+//    allowedHosts: 'all',
+    host: '0.0.0.0',
     port: 1338,
     historyApiFallback: true,
     publicPath: '/',
